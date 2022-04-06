6af1f2591d21c6e7328b724931f1aa88b6bd5cd9
oyj01 <1995ouyangjun@gmail.com>
2022-04-06 06:44:30 +0000

    update 2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,10 +1,10 @@
 def FizzBuzz(num)
-  if num % 3 == 0
-      return 'Fizz'
+  if num % 3 == 0 && num % 5 == 0
+      return 'FizzBuzz'
     elsif num % 5 == 0
       return 'Buzz'
-    elsif num % 3 == 0 && num % 5 == 0
-      return 'FizzBuzz'
+    elsif num % 3 == 0
+      return 'Fizz'
     else
       return num
   end
